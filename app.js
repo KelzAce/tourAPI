d23b897eee85aad8f39f8e9865fc6111dbee07d4
ikechi Greatness <ikechigreat@gmail.com>
2023-02-24 02:37:17 +0100

    Security

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -13,9 +18,52 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json());
+//GLOBAL RATE LIMIT
+//SET SECURITY HTTP HEADERS
+app.use(helmet());
+
+//DEVELOPMENT LOGGING
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//LIMIT REQUEST FROM SAME API
+const limiter = rateLimit({
+  //100 request per hour
+  max: 100,
+  //1hour
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this IP, please try again in an hour",
+});
+
+app.use("/api", limiter);
+
+//BODY PARSER, READING DATA FROM BODY INTO REQ.BODY
+app.use(express.json({ limit: "10kb" }));
+
+//DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSanitize());
+
+//DATA SANITIZATION AGAINST XSS(CROSS SITE SCRIPTING)
+app.use(xss());
+
+//Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
+
 app.use(express.static(`${__dirname}/public`));
 
+//SERVIN STATIC FILES
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,23 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    //cookie sent on encrypted connection i.e https
+    // secure: true,
+    //cookie wont be accessed or modified in anyway by the browser
+    httpOnly: true,
+  };
+
+  res.cookie("jwt", token, cookieOptions);
+
+  //remove the password from the output
+  user.password = undefined;
+
+  if (process.env.NODE_ENV !== "production") cookieOptions.secure = true;
+
   res.status(statusCode).json({
     status: "success",
     token,

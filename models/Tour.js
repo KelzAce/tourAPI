e2d083b35ab127fc4947344e6f19495f3fb5e17c
ikechi Greatness <ikechigreat@gmail.com>
2023-02-28 09:33:25 +0100

    Embedding a location document into the parent Tour Doc.

--- models/Tour.js
+++ models/Tour.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const User = require("./User");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -80,6 +81,36 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      //Geojson
+      type: {
+        type: String,
+        default: "Point",
+        enum: ["Point"],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: "Point",
+          enum: ["Point"],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectID,
+        ref: "User",
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -97,8 +128,10 @@ tourSchema.pre("save", function (next) {
   next();
 });
 
-// tourSchema.post("save", function (doc, next) {
-//   console.log(doc);
+// // EMBEDDING LOCATION LOGIC
+// tourSchema.pre("save", async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
 //   next();
 // });
 

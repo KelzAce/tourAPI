104ad609a7490d258eeb8174e03dc242db0c830f
ikechi Greatness <ikechigreat@gmail.com>
2023-03-02 10:46:25 +0100

    Nesting Routes

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -2,10 +2,10 @@ const express = require("express");
 const reviewController = require("../controllers/reviewController");
 const authController = require("../controllers/authController");
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router
-  .route("./")
+  .route("/")
   .get(reviewController.getAllReviews)
   .post(
     authController.protect,
